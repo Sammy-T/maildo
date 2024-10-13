811f7d6ccd606b8499726d90a20e9addc6ecfb09
Sammy-T <cyam9019@gmail.com>
2024-10-13 00:28:27 -0400

    Create app initialization

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-import './app.css'
-import App from './App.svelte'
+import './app.css';
+import App from './App.svelte';
 
-const app = new App({
-  target: document.getElementById('app'),
-})
+const APP_ID = 'maildo-app';
+
+/** @type {App} */
+let app;
+
+function init() {
+    let targetEl = document.getElementById(APP_ID);
+
+    if(!targetEl) {
+        targetEl = document.createElement('slot');
+        targetEl.id = APP_ID;
+
+        document.body.appendChild(targetEl);
+    } else if(targetEl.childElementCount > 0) {
+        console.warn('App target already exists');
+        return;
+    }
+
+    app = new App({
+        target: targetEl
+    });
+}
+
+init();
 
 export default app
