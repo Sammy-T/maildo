830a1a05845c2286a6588e89a89756f261f30cf8
Sammy-T <cyam9019@gmail.com>
2024-10-13 16:00:45 -0400

    Ignore excluded links

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,14 @@
     import { onMount, setContext } from 'svelte';
     import { writable } from 'svelte/store';
 
+    const MAILTO_SELECTORS = 'a[href^="mailto:"]:not(.no-maildo), a[href="#maildo"], a.maildo';
+
     /** @type {import('svelte/store').Writable<HTMLAnchorElement>} */
     const selectedMailto = writable(null);
     setContext('selectedMailto', selectedMailto);
     
     onMount(() => {
-        const mailtos = document.querySelectorAll('a[href^="mailto:"], a[href="#maildo"], a.maildo');
+        const mailtos = document.querySelectorAll(MAILTO_SELECTORS);
         
         mailtos.forEach((mailto) => {
             mailto.addEventListener('click', (event) => {
