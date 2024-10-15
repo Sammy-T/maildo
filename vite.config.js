5de81dab30b1b0444a4f3acb10a35d396332912c
Sammy-T <cyam9019@gmail.com>
2024-10-15 18:53:26 -0400

    Configure build output

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        assetFileNames: '[name][extname]',
+        chunkFileNames: '[name].js',
+      }
+    }
+  }
 })
